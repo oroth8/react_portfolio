8e04d68b0c8f993d0c7cbd5a2d58471fd8b838bf
Owen Roth <rothowen27@gmail.com>
2020-11-24 15:38:44 -0600

    more styling added google fonts

--- client/src/components/ServiceSection.js
+++ client/src/components/ServiceSection.js
@@ -5,49 +5,83 @@ import money from '../img/money.svg';
 import teamwork from '../img/teamwork.svg'
 import home2 from '../img/home2.png'
 
+import styled from 'styled-components';
+
+import {About, Description, Image, Hide} from '../styles';
+
 
 const ServiceSection = () => {
     return (
-        <div className='services'>
-            <div className='description'>
+        <Services>
+            <Description>
                 <h2>High <span>quality</span> services</h2>
-                <div className='cards'>
-                    <div className='card'>
+                <Cards>
+                    <Card>
                         <div className='icon'>
                             <img src={clock} alt='clock'/>
                             <h3>Effecient</h3>
                         </div>
                         <p>Lorem asdasdasd asdasdasdlkj asdasdasd</p>
-                    </div>
-                    <div className='card'>
+                    </Card>
+                    <Card>
                         <div className='icon'>
                             <img src={teamwork} alt='teamwork'/>
                             <h3>Teamwork</h3>
                         </div>
                         <p>Lorem asdasdasd asdasdasdlkj asdasdasd</p>
-                    </div>
-                    <div className='card'>
+                    </Card>
+                    <Card>
                         <div className='icon'>
                             <img src={diaphragm} alt='diaphragm'/>
                             <h3>diaphragm</h3>
                         </div>
                         <p>Lorem asdasdasd asdasdasdlkj asdasdasd</p>
-                    </div>
-                    <div className='card'>
+                    </Card>
+                    <Card>
                         <div className='icon'>
                             <img src={money} alt='money'/>
                             <h3>Affordable</h3>
                         </div>
                         <p>Lorem asdasdasd asdasdasdlkj asdasdasd</p>
-                    </div>
-                </div>
-                <div className='image'>
+                    </Card>
+                </Cards>
+                </Description>
+                <Image>
                     <img src={home2} alt='home2'/>
-                </div>
-            </div>
+                </Image>
             
-        </div>
+        </Services>
     )
 }
+
+// style
+const Services = styled(About)`
+/* naming convention */
+h2{
+    padding-bottom: 5rem;
+}
+p{
+    width: 70%;
+    padding: 2rem 0rem 4rem 0rem;
+}
+`
+const Cards = styled.div`
+display: flex;
+flex-wrap: wrap;
+`
+
+const Card = styled.div` 
+flex-basis: 20rem;
+    .icon{
+        display: flex;
+        align-items: center;
+        h3{
+            margin-left: 1rem;
+            background: white;
+            color: black;
+            padding: 1rem;
+        }
+    }
+`
 
 export default ServiceSection
