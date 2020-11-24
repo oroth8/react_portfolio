1e59073994706994576871af1a9b9334db00fa5a
Owen Roth <rothowen27@gmail.com>
2020-11-24 15:49:36 -0600

    added faq styling

--- client/src/components/FaqSection.js
+++ client/src/components/FaqSection.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import styled from 'styled-components';
+import {About} from '../styles';
 
 const FaqSection = () => {
     return (
-        <div className='faq'>
+        <Faq>
             <h2>Any Questions <span>FAQ</span></h2>
             <div className='question'>
                 <h4>How do I start?</h4>
@@ -10,6 +12,7 @@ const FaqSection = () => {
                     <p>Lorem ipsum nesciunt.</p>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, nisi?</p>
                 </div>
+                <div className="faq-line"></div>
             </div>
             <div className='question'>
                 <h4>Daily Schedule</h4>
@@ -17,6 +20,7 @@ const FaqSection = () => {
                     <p>Lorem ipsum nesciunt.</p>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, nisi?</p>
                 </div>
+                <div className="faq-line"></div>
             </div>
             <div className='question'>
                 <h4>Payment Methods</h4>
@@ -24,6 +28,7 @@ const FaqSection = () => {
                     <p>Lorem ipsum nesciunt.</p>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, nisi?</p>
                 </div>
+                <div className="faq-line"></div>
             </div>
             <div className='question'>
                 <h4>Services Offered</h4>
@@ -31,9 +36,38 @@ const FaqSection = () => {
                     <p>Lorem ipsum nesciunt.</p>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, nisi?</p>
                 </div>
+                <div className="faq-line"></div>
             </div>
-        </div>
+        </Faq>
     )
 }
+
+const Faq = styled(About)`
+/* naming convention */
+display: block;
+span{
+    display: block;
+}
+h2{
+    padding-bottom: 2rem;
+    font-weight: lighter;
+}
+.faq-line{
+    background: #cccccc;
+    height: 0.2rem;
+    margin: 2rem 0rem;
+    width: 100%;
+}
+.question{
+    padding: 3rem 0rem;
+    cursor: pointer;
+}
+.answer{
+    padding: 2rem 0rem;
+    p{
+    padding: 1rem 0rem;
+    }
+}
+`;
 
 export default FaqSection;
