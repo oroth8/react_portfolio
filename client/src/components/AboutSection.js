04a01ca99a4e13df45dd9401504336770fa06791
Owen Roth <rothowen27@gmail.com>
2020-11-24 21:06:00 -0600

    added line animation on about section

--- client/src/components/AboutSection.js
+++ client/src/components/AboutSection.js
@@ -3,6 +3,8 @@ import home1 from "../img/home1.png";
 // import styled from 'styled-components';
 import { About, Description, Image, Hide } from "../styles";
 import { motion } from "framer-motion";
+import { fade, titleAnim, photoAnim } from "../animation";
+import Wave from "./Wave";
 
 const AboutSection = () => {
   return (
@@ -10,23 +12,26 @@ const AboutSection = () => {
       <Description>
         <motion.div>
           <Hide>
-            <motion.h2>Owen Roth</motion.h2>
+            <motion.h2 variants={titleAnim}>Owen Roth</motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>
+            <motion.h2 variants={titleAnim}>
               Full Stack <span>Developer</span>
             </motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>Salesforce Admin</motion.h2>
+            <motion.h2 variants={titleAnim}>Salesforce Admin</motion.h2>
           </Hide>
         </motion.div>
-        <p>Contact me for any contract work or job inquirerys.</p>
-        <button>Contact Me</button>
+        <motion.p variants={fade}>
+          Contact me for any contract work or job inquirerys.
+        </motion.p>
+        <motion.button variants={fade}>Contact Me</motion.button>
       </Description>
       <Image>
-        <img src={home1} alt="Profile" />
+        <motion.img variants={photoAnim} src={home1} alt="Profile" />
       </Image>
+      <Wave />
     </About>
   );
 };
