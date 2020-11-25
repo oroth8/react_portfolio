e9cbba3f31b84665cd2e780f72e0cbf2059d228e
Owen Roth <rothowen27@gmail.com>
2020-11-24 22:59:22 -0600

    added contact page holders and scroll animation for faq/service section

--- client/src/components/ServiceSection.js
+++ client/src/components/ServiceSection.js
@@ -1,87 +1,94 @@
-import React from 'react'
-import clock from '../img/clock.svg';
-import diaphragm from '../img/diaphragm.svg';
-import money from '../img/money.svg';
-import teamwork from '../img/teamwork.svg'
-import home2 from '../img/home2.png'
+import React from "react";
+import clock from "../img/clock.svg";
+import diaphragm from "../img/diaphragm.svg";
+import money from "../img/money.svg";
+import teamwork from "../img/teamwork.svg";
+import home2 from "../img/home2.png";
 
-import styled from 'styled-components';
-
-import {About, Description, Image, Hide} from '../styles';
-
+import styled from "styled-components";
+import { About, Description, Image } from "../styles";
+import { scrollReveal } from "../animation";
+import { useScroll } from "./useScroll";
 
 const ServiceSection = () => {
-    return (
-        <Services>
-            <Description>
-                <h2>High <span>quality</span> services</h2>
-                <Cards>
-                    <Card>
-                        <div className='icon'>
-                            <img src={clock} alt='clock'/>
-                            <h3>Effecient</h3>
-                        </div>
-                        <p>Lorem asdasdasd asdasdasdlkj asdasdasd</p>
-                    </Card>
-                    <Card>
-                        <div className='icon'>
-                            <img src={teamwork} alt='teamwork'/>
-                            <h3>Teamwork</h3>
-                        </div>
-                        <p>Lorem asdasdasd asdasdasdlkj asdasdasd</p>
-                    </Card>
-                    <Card>
-                        <div className='icon'>
-                            <img src={diaphragm} alt='diaphragm'/>
-                            <h3>diaphragm</h3>
-                        </div>
-                        <p>Lorem asdasdasd asdasdasdlkj asdasdasd</p>
-                    </Card>
-                    <Card>
-                        <div className='icon'>
-                            <img src={money} alt='money'/>
-                            <h3>Affordable</h3>
-                        </div>
-                        <p>Lorem asdasdasd asdasdasdlkj asdasdasd</p>
-                    </Card>
-                </Cards>
-                </Description>
-                <Image>
-                    <img src={home2} alt='home2'/>
-                </Image>
-            
-        </Services>
-    )
-}
+  const [element, controls] = useScroll();
+  return (
+    <Services
+      variants={scrollReveal}
+      animate={controls}
+      initial="hidden"
+      ref={element}
+    >
+      <Description>
+        <h2>
+          High <span>quality</span> services
+        </h2>
+        <Cards>
+          <Card>
+            <div className="icon">
+              <img src={clock} alt="clock" />
+              <h3>Effecient</h3>
+            </div>
+            <p>Lorem asdasdasd asdasdasdlkj asdasdasd</p>
+          </Card>
+          <Card>
+            <div className="icon">
+              <img src={teamwork} alt="teamwork" />
+              <h3>Teamwork</h3>
+            </div>
+            <p>Lorem asdasdasd asdasdasdlkj asdasdasd</p>
+          </Card>
+          <Card>
+            <div className="icon">
+              <img src={diaphragm} alt="diaphragm" />
+              <h3>diaphragm</h3>
+            </div>
+            <p>Lorem asdasdasd asdasdasdlkj asdasdasd</p>
+          </Card>
+          <Card>
+            <div className="icon">
+              <img src={money} alt="money" />
+              <h3>Affordable</h3>
+            </div>
+            <p>Lorem asdasdasd asdasdasdlkj asdasdasd</p>
+          </Card>
+        </Cards>
+      </Description>
+      <Image>
+        <img src={home2} alt="home2" />
+      </Image>
+    </Services>
+  );
+};
 
 // style
 const Services = styled(About)`
-/* naming convention */
-h2{
+  /* naming convention */
+  h2 {
     padding-bottom: 5rem;
-}
-p{
+  }
+  p {
     width: 70%;
     padding: 2rem 0rem 4rem 0rem;
-}
-`
+  }
+`;
 const Cards = styled.div`
-display: flex;
-flex-wrap: wrap;
-`
+  display: flex;
+  flex-wrap: wrap;
+`;
 
-const Card = styled.div` 
-flex-basis: 20rem;
-    .icon{
-        display: flex;
-        align-items: center;
-        h3{
-            margin-left: 1rem;
-            background: white;
-            color: black;
-            padding: 1rem;
-        }
+const Card = styled.div`
+  flex-basis: 20rem;
+  .icon {
+    display: flex;
+    align-items: center;
+    h3 {
+      margin-left: 1rem;
+      background: white;
+      color: black;
+      padding: 1rem;
     }
-`
+  }
+`;
 
-export default ServiceSection
+export default ServiceSection;
