7611991dd0a218a114b9988df65983c9ae6a3ad4
Owen Roth <rothowen27@gmail.com>
2020-11-24 22:25:55 -0600

    added faq animation and toggle feature. added animations to work

--- client/src/components/FaqSection.js
+++ client/src/components/FaqSection.js
@@ -1,73 +1,83 @@
-import React from 'react'
-import styled from 'styled-components';
-import {About} from '../styles';
+import React from "react";
+import styled from "styled-components";
+import { About } from "../styles";
+import Toggle from "./Toggle";
+import { AnimateSharedLayout } from "framer-motion";
 
 const FaqSection = () => {
-    return (
-        <Faq>
-            <h2>Any Questions <span>FAQ</span></h2>
-            <div className='question'>
-                <h4>How do I start?</h4>
-                <div className='answer'>
-                    <p>Lorem ipsum nesciunt.</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, nisi?</p>
-                </div>
-                <div className="faq-line"></div>
-            </div>
-            <div className='question'>
-                <h4>Daily Schedule</h4>
-                <div className='answer'>
-                    <p>Lorem ipsum nesciunt.</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, nisi?</p>
-                </div>
-                <div className="faq-line"></div>
-            </div>
-            <div className='question'>
-                <h4>Payment Methods</h4>
-                <div className='answer'>
-                    <p>Lorem ipsum nesciunt.</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, nisi?</p>
-                </div>
-                <div className="faq-line"></div>
-            </div>
-            <div className='question'>
-                <h4>Services Offered</h4>
-                <div className='answer'>
-                    <p>Lorem ipsum nesciunt.</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, nisi?</p>
-                </div>
-                <div className="faq-line"></div>
-            </div>
-        </Faq>
-    )
-}
+  return (
+    <Faq>
+      <h2>
+        Any Questions <span>FAQ</span>
+      </h2>
+      <AnimateSharedLayout>
+        <Toggle title={"How do I start?"}>
+          <div className="answer">
+            <p>Lorem ipsum nesciunt.</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Laboriosam, nisi?
+            </p>
+          </div>
+        </Toggle>
+        <Toggle title={"Daily Schedule"}>
+          <div className="answer">
+            <p>Lorem ipsum nesciunt.</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Laboriosam, nisi?
+            </p>
+          </div>
+        </Toggle>
+        <Toggle title={"Payment Methods"}>
+          <div className="answer">
+            <p>Lorem ipsum nesciunt.</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Laboriosam, nisi?
+            </p>
+          </div>
+        </Toggle>
+        <Toggle title={"Services Offered"}>
+          <div className="answer">
+            <p>Lorem ipsum nesciunt.</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Laboriosam, nisi?
+            </p>
+          </div>
+        </Toggle>
+      </AnimateSharedLayout>
+    </Faq>
+  );
+};
 
 const Faq = styled(About)`
-/* naming convention */
-display: block;
-span{
+  /* naming convention */
+  display: block;
+  span {
     display: block;
-}
-h2{
+  }
+  h2 {
     padding-bottom: 2rem;
     font-weight: lighter;
-}
-.faq-line{
+  }
+  .faq-line {
     background: #cccccc;
     height: 0.2rem;
     margin: 2rem 0rem;
     width: 100%;
-}
-.question{
+  }
+  .question {
     padding: 3rem 0rem;
     cursor: pointer;
-}
-.answer{
+  }
+  .answer {
     padding: 2rem 0rem;
-    p{
-    padding: 1rem 0rem;
+    p {
+      padding: 1rem 0rem;
     }
-}
+  }
 `;
 
 export default FaqSection;
